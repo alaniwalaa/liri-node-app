af90227e98c28984eadc330c2774f75eb55552c7
walaa <w.alani2010@yahoo.com>
2019-08-30 18:04:07 -0700

    main files been created and the liri file outlined with the 4 functions - two of the functions been completed (movie-this and concert-this)

---
+++ keys.js
@@ -0,0 +1,20 @@
+Brands in Town API: 
+Spotify API: 
+OMDB API: 
+
+
+
+// TO INSTALL SPOTIFY
+npm i node-spotify-api
+
+// TO INSTALL AXIOS 
+npm i axios
+
+// OMDB API KEY 
+http://www.omdbapi.com/?i=tt3896198&apikey=54109c97
+
+// TO INSTALL MOMENT 
+npm i moment
+
+// TO INSTALL DOTENV
+npm i dotenv
---
+++ liriNode.js
@@ -0,0 +1,18 @@
+
+// project variables 
+var keys = require("./keys.js");
+var fs = require("fs");
+var liriReturn = process.argv[2];
+
+// switches for various commands 
+switch(liriReturn) {
+    case 
+
+    break;
+    case 
+
+    break;
+
+    
+}
+
